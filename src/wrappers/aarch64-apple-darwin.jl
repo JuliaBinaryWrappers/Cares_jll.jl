149ce60b6dc0925ddaf2ab792e354dedd39f3374
jlbuild <juliabuildbot@gmail.com>
2024-01-27 23:41:31 +0000

    Cares_jll build 1.26.0+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -9,7 +9,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libcares,
-        "lib/libcares.2.10.1.dylib",
+        "lib/libcares.2.11.0.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
